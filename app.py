8a2f74c1edb32a04c7e93a051fab97fa6d858d5e
gmrabbi <gmrabbi91221@gmail.com>
2026-02-13 02:44:58 +0600

    Successfully its working

---
+++ app.py
@@ -0,0 +1,20 @@
+import numpy as np 
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open("model/crop_model.pkl", "rb"))
+
+@app.route("/")
+def Home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    float_feature = [float(x) for x in request.form.values()]
+    features = [np.array(float_feature)]
+    prediction = model.predict(features)[0]
+    return render_template('index.html', prediction_text=f"The Predicted Crop is {prediction}")
+
+if __name__ == "__main__":
+    app.run(debug=True)
